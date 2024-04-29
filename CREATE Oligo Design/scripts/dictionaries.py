cde108eeb36417c8a64565678c9ac3f31b7013ee
SevvalliThavapalan <64902274+SevvalliThavapalan@users.noreply.github.com>
2024-04-29 10:30:56 +0200

    CREATE
    Added scripts for CREATE

---
+++ CREATE Oligo Design/scripts/dictionaries.py
@@ -0,0 +1,125 @@
+
+
+three_one = { "A" : "ALA", "G" : "GLY", "I" : "ILE", "L" : "LEU", "P" : "PRO",
+              "V" : "VAL", "F" : "PHE", "W" : "TRP", "Y" : "TYR", "D" : "ASP",
+              "E" : "GLU", "R" : "ARG", "H" : "HIS", "K" : "LYS", "S" : "SER",
+              "T" : "THR", "C" : "CYS", "M" : "MET", "N" : "ASN", "Q" : "GLN"
+    }
+
+aa_nt = { "ALA" : ["GCT","GCC","GCA","GCG"], "GLY" : ["GGT","GGC","GGA","GGG"], "ILE" : ["ATT","ATC","ATA"],
+          "LEU" : ["CTT","CTC","CTA","CTG","TTG","TTA"], "VAL" : ["GTT","GTC","GTA","GTG"], "PHE" : ["TTT","TTC"],
+          "TRP" : ["TGA","TGG"], "TYR" : ["TAT","TAC"], "ASP" : ["GAT","GAC"], "GLU" : ["GAA","GAG"],
+          "ARG" : ["AGA","AGG", "CGT", "CGC", "CGA", "CGG"], "HIS" : ["CAT", "CAC"], "LYS" : ["AAA","AAG"], "SER" : ["AGT","AGC","TCT","TCC", "TCA", "TCG"],
+          "THR" : ["ACT","ACC","ACA","ACG"], "CYS" : ["TGT","TGC"], "MET" : ["ATG"], 
+          "ASN" : ["AAT", "AAC"], "GLN" : ["CAA","CAG"], "PRO" : ["CCT","CCC","CCA","CCG"]
+    }
+
+base_pairs = {"A" : "T",
+              "G" : "C",
+              "C" : "G",
+              "T" : "A"
+    }
+
+
+codon_usage = { "GGG" : 0.13, "GGA" : 0.09,"GGC" : 0.4, "GGT" : 0.38, #Gly
+                "AGG" : 0.03, "AGA" : 0.04,  #Arg
+                "CGG" : 0.08, "CGA" : 0.05,"CGC" : 0.37, "CGT" : 0.42,
+                "CCC" : 0.1,  "CCT" : 0.16,"CCA" : 0.2,"CCG" : 0.55,#Pro
+                "TGG" : 1.0,   #Trp
+                "ACC" : 0.43, "ACT" : 0.21,"ACA" : 0.3,"ACG" : 0.23, #Thr
+                "GCT" : 0.19, "GCC" : 0.25, "GCA" : 0.22, "GCG" : 0.34, #Ala
+                "TCT" : 0.19, "TCC" : 0.17, "TCA" : 0.12, "TCG" : 0.13, #Ser
+                "AGC" : 0.27, "AGT" : 0.13,
+                "CTT" : 0.1,  "CTC": 0.1, "CTA" : 0.3, "CTG" : 0.55, #Leu
+                "TTA" : 0.11, "TTG" : 0.11,
+                "ATG" : 1, #Met
+                "GTT" : 0.29, "GTC" : 0.2, "GTG" : 0.34, "GTA" : 0.17, #Val
+                "CAA" : 0.31, "CAG" : 0.69, #Gln
+                "AAA" : 0.76, "AAG" : 0.24, #Lys
+                "GAA" : 0.7, "GAG": 0.3, #Glu
+                "ATT" : 0.47, "ATC": 0.46, "ATA": 0.07, #Ile
+                "TAT" : 0.53, "TAC": 0.47, #Tyr
+                "CAT" : 0.52, "CAC": 0.48, #His
+                "AAT" : 0.39, "AAC" : 0.61, #Asn
+                "GAT" : 0.59, "GAC" : 0.41, #Asp
+                "TGT" : 0.43, "TGC" : 0.57 # Cys
+}
+
+
+#NGG, CCN shift 0
+substitution_1 = {"CGG" : "CGT",
+                  "AGG" : "AGA",
+                  "GGG" : "GGT"}
+
+# NNG, NCC shift +1
+substitution_nng = {"TTG" : "TTA",
+                  "CTG" : "CTA",
+                  "GTG" : "GTT",
+                  "TCG" : "TCA",
+                  "ACG" : "ACT",
+                  "GCG" : "GCA",
+                  "CAG" : "CAA",
+                  "AAG" : "AAA",
+                  "GAG" : "GAA",
+                  "CGG" : "CGA",
+                  "AGG" : "AGA",
+                  "GGG" : "GGA"}
+
+substitution_ncc = {"TCC" : "TCT",
+                    "ACC" : "ACT",
+                    "GCC" : "GCA"}
+
+#shift +2 
+#bei negativen Distanzen
+substitution_cnn = {"CTA" : "TTA",
+                    "CTG" : "TTG",
+                    "CGA" : "AGA",
+                    "CGG" : "AGG"}
+
+#bei positiven Distanzen
+substitution_nnc = { "TTC" : "TTT",
+                     "CTC" : "CTT",
+                     "ATC" : "ATT",
+                     "GTC" : "GTA",
+                     "TCC" : "TCT",
+                     "ACC" : "ACA",
+                     "GCC" : "GCA",
+                     "TAC" : "TAT",
+                     "CAC" : "CAT",
+                     "AAC" : "AAT",
+                     "GAC" : "GAT",
+                     "TGC" : "TGT",
+                     "CGC" : "CGA",
+                     "AGC" : "AGT",
+                     "GGC" : "GGT"}
+
+  #shift +2 
+    #bei negativen Distanzen
+substitution_cnn = {"CTA" : "TTA",
+                    "CTG" : "TTG",
+                    "CGA" : "AGA",
+                    "CGG" : "AGG"}
+
+codon_usage = { "GGG" : 0.13, "GGA" : 0.09,"GGC" : 0.4, "GGT" : 0.38, #Gly
+                "AGG" : 0.03, "AGA" : 0.04,  #Arg
+                "CGG" : 0.08, "CGA" : 0.05,"CGC" : 0.37, "CGT" : 0.42,
+                "CCC" : 0.1,  "CCT" : 0.16,"CCA" : 0.2,"CCG" : 0.55,#Pro
+                "TGG" : 1.0,   #Trp
+                "ACC" : 0.43, "ACT" : 0.21,"ACA" : 0.3,"ACG" : 0.23, #Thr
+                "GCT" : 0.19, "GCC" : 0.25, "GCA" : 0.22, "GCG" : 0.34, #Ala
+                "TCT" : 0.19, "TCC" : 0.17, "TCA" : 0.12, "TCG" : 0.13, #Ser
+                "AGC" : 0.27, "AGT" : 0.13,
+                "CTT" : 0.1,  "CTC": 0.1, "CTA" : 0.3, "CTG" : 0.55, #Leu
+                "TTA" : 0.11, "TTG" : 0.11,
+                "ATG" : 1, #Met
+                "GTT" : 0.29, "GTC" : 0.2, "GTG" : 0.34, "GTA" : 0.17, #Val
+                "CAA" : 0.31, "CAG" : 0.69, #Gln
+                "AAA" : 0.76, "AAG" : 0.24, #Lys
+                "GAA" : 0.7, "GAG": 0.3, #Glu
+                "ATT" : 0.47, "ATC": 0.46, "ATA": 0.07, #Ile
+                "TAT" : 0.53, "TAC": 0.47, #Tyr
+                "CAT" : 0.52, "CAC": 0.48, #His
+                "AAT" : 0.39, "AAC" : 0.61, #Asn
+                "GAT" : 0.59, "GAC" : 0.41, #Asp
+                "TGT" : 0.43, "TGC" : 0.57 # Cys
+    }
---
+++ CREATE Oligo Design/scripts/write_df.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 12:37:26 2022
+
+@author: Sevvalli Thavapalan
+"""
+import pandas as pd
+import dictionaries
+
+
+def get_keys_by_value(my_dict, search_value):
+    keys_with_value = []
+    for key, values_list in my_dict.items():
+        if search_value in values_list:
+            keys_with_value.append(key)
+    return keys_with_value
+
+# Function to clean the values in the specified columns
+def clean_values(cell_value):
+    if pd.notna(cell_value):
+        return str(cell_value).strip("[]").replace("'", "")
+    return cell_value
+
+def process_gene(gene, dictionary_values):
+    if gene:
+        return gene[0]
+        
+    else:
+        print(gene, dictionary_values)
+        for key, values in dictionaries.aa_nt.items():
+                if dictionary_values in values:
+                    return key
+    return None
+
+
+def write_dataframe(name,example_gene, reduced_dict):
+    base_pairs = {"A" : "T",
+                  "G" : "C",
+                  "C" : "G",
+                  "T" : "A"
+        }
+    sub_library_spacer = "TCCTCTGGCGGAAAGCCT"
+    spacer = "GATC"
+    pj23119 = "ttgacagctagctcagtcctaggtataatactagt"
+    cas_handle = "gttttagagctagaaatagcaagttaaaataaggctag"
+    oligo_df = pd.DataFrame()
+    position_aa = []
+    position_nt = []
+    homology_arm = []
+    protospacer = []
+    distance = []
+    pam_mutation = []
+    mutated_pam = []
+    parent_codon = []
+    child_codon = []
+    oligo = []
+    mutated_codon = []
+    target_strand = []
+    for key, value in reduced_dict.items():
+        #print(key, value)
+        position = (key -42)
+       
+        for entry in value:
+            if len(entry) > 5:
+                position_aa.append(position/3+1)
+                parent_codon.append(entry[3])
+                child_codon.append(entry[4])
+                position_nt.append(position+1)
+                distance.append(entry[1])
+                homology_arm.append(entry[5])
+                pam_mutation.append(entry[0])
+                mutated_pam.append(entry[6])
+                mutated_codon.append(entry[7])
+                if entry[0].startswith("CC"): #non template coding strand
+                    if entry[1] >=0:  # positive disatances
+                        pos = (key-1) + (entry[1])
+                        complement= ""
+                        if (entry[1]%3) ==0: #
+                            complement = example_gene[pos+3:pos+23]
+                        elif (entry[1]%2) ==0:
+                            complement = example_gene[pos+2:pos+22]         
+                        else:
+                            complement = example_gene[pos+3:pos+23]
+                        rev_complement = ""
+                        for char in complement:
+                            rev_complement += base_pairs[char]
+                        protospacer.append(rev_complement[::-1])
+                        oligo.append(sub_library_spacer + entry[5] + spacer + pj23119 + rev_complement[::-1] + cas_handle)
+                        #print(rev_complement[::-1])
+                        target_strand.append("non template / coding strand")
+                    else: # negative distances
+                        pos = (key) + (entry[1])
+                        complement= ""
+                        if (entry[1]%3) == 0:
+                            complement = example_gene[pos+2:pos+22]
+                        elif(entry[1]%2)==0:
+                            complement = example_gene[pos+2:pos+22]
+                        else:
+                            complement = example_gene[pos+2:pos+22]
+                            #print(entry[1])
+                            #print(entry[5])
+                            #print(complement)
+                        rev_complement = ""
+                        for char in complement:
+                            rev_complement += base_pairs[char]
+                        protospacer.append(rev_complement[::-1])
+                        oligo.append(sub_library_spacer + entry[5] + spacer + pj23119 + rev_complement[::-1] + cas_handle)
+                        #print(rev_complement[::-1])
+                        target_strand.append("non template / coding strand")
+                        
+                else: #template coding strand
+                    pos = (key-1) + (entry[1])
+                    if entry[1] >=0:  # positive disatances
+                        complement=""
+                        if (entry[1]%3) ==0: #
+                            complement = example_gene[pos-20:pos]
+                        elif (entry[1]%2) ==0:
+                            complement = example_gene[pos-20:pos]      
+                        else:
+                            complement = example_gene[pos-20:pos]
+                        protospacer.append(complement)
+                        #print(len(complement))
+                        target_strand.append("template / non coding strand")
+                        oligo.append(sub_library_spacer + entry[5] + spacer + pj23119 + complement + cas_handle)
+                    else:
+                         # negative distances
+                        pos = (key) + (entry[1])
+                        complement= ""
+                        if (entry[1]%3) == 0:
+                            complement = example_gene[pos-20:pos]
+                        elif(entry[1]%2)==0:
+                            complement = example_gene[pos-21:pos-1]
+                        else:
+                            complement = example_gene[pos-21:pos-1]
+                        protospacer.append(complement)
+                        #print(len(complement))
+                     
+                        target_strand.append("template / non coding strand")
+                        oligo.append(sub_library_spacer + entry[5] + spacer + pj23119 + complement + cas_handle)
+    mutated_aa = []
+    for i in child_codon:
+        keys_for_codon = get_keys_by_value(dictionaries.aa_nt, i)
+        mutated_aa.append(keys_for_codon)
+    
+    parent_aa = []
+    for j in parent_codon:
+        keys_for_codon = get_keys_by_value(dictionaries.aa_nt, j)
+        parent_aa.append(keys_for_codon)
+    
+    gene_name = []
+    for i in range(len(parent_aa)):
+        gene_name.append(name)
+                
+    oligo_df["gene"] = gene_name
+    oligo_df["parent aa"] = parent_aa
+    oligo_df["parent codon"] = parent_codon
+    oligo_df["aa position"] = position_aa
+    oligo_df["mutated aa"]  = mutated_aa 
+    oligo_df["child codon"] = child_codon        
+    oligo_df["nt position"] = position_nt
+    oligo_df["dist mut pam"] = distance
+    oligo_df["pam"] = pam_mutation
+    oligo_df["mutated pam"] = mutated_pam
+    oligo_df["homology arm"] = homology_arm
+    oligo_df["target strand of protospacer"] = target_strand
+    oligo_df["protospacer"] = protospacer
+    oligo_df["oligo"] = oligo
+
+    
+    oligo_df = oligo_df.drop(oligo_df[oligo_df['protospacer'].map(len) < 20].index)
+    oligo_df = oligo_df.drop(oligo_df[oligo_df['oligo'].map(len) < 150].index)
+    columns_to_clean = ["parent aa", "mutated aa"]
+    # Check if values in Column_A and Column_B are the same
+    mask = oligo_df['parent aa'] == oligo_df['mutated aa']
+
+    # Remove rows where values in Column_A and Column_B are the same
+    oligo_df = oligo_df[~mask]
+    oligo_df[columns_to_clean] = oligo_df[columns_to_clean].applymap(clean_values)
+    oligo_df = oligo_df[~oligo_df['parent codon'].isin(['TAA', 'TAG'])]
+    
+
+    return oligo_df
+
+if __name__ == "__main__":
+    write_df()	
