5d74ac4a5952798697e73d9495c9a48b45ef2b12
SevvalliThavapalan <64902274+SevvalliThavapalan@users.noreply.github.com>
2025-04-24 09:24:20 +0200

    Update Introduction.py

--- Web_Application/Introduction.py
+++ Web_Application/Introduction.py
@@ -14,15 +14,15 @@ st.title("Introduction")
 
 st.markdown("""
 
-This web application was developed to translate a list of amino acid mutations into DNA oligomers with sgRNA-insert pairs for CRISPR assisted recombineering. It is part of the project *A repository-scale workflow for linking bacterial 
-            genetic variation to phenotypes*. The aim of the workflow is to systematically identify, introduce and analyse effects of individual mutations. Purpose of this web application is the second step of the workflow, enabling the 
+This web application was developed to translate a list of amino acid mutations into DNA oligomers with sgRNA-insert pairs for CRISPR assisted recombineering. It is part of the project *"A repository-scale workflow for linking bacterial 
+            genetic variation to phenotypes"*. The aim of the workflow is to systematically identify, introduce and analyse effects of individual mutations. Purpose of this web application is the second step of the workflow, enabling the 
             automated design of sgRNA-insert pairs. The design principles are based on [Garst et al.](https://www.nature.com/articles/nbt.3718) and the adapted version of another study by our [group](https://www.embopress.org/doi/full/10.15252/msb.202311596).
 """)
 
 st.markdown("## Functions of the web application")
 st.markdown(
     """
-    - **Design sgRNA and insert sequences** for a list of amino acid mutations.
+    - **Design sgRNA-insert pair sequences** for a list of amino acid mutations.
     - **Mutagenesis** of a single protein sequence. The resulting table \
     can be used to design sgRNA-insert pairs.
     - **Off target finder** for a list of base pairing regions. \
@@ -36,8 +36,8 @@ st.markdown("### Limitations")
 st.markdown(
     """
     - The web application has a 200 MB data limit. Larger datasets might lead to \
-    issues such as long runtimes or crushing the server.
-    - This part of the workflow only allows to generate and design sgRNA and insert \
+    issues.
+    - This part of the workflow only allows to generate and design sgRNA-insert \
     pools. Sequenced data to investigate the library composition can not be processed here. \
     But there are scripts available on GitHub and can be cloned to perform the processing.
     """
