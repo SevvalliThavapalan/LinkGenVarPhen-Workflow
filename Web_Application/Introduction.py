9929ced0f25f0c2154e6cf49ef49a0401edc5cfd
SevvalliThavapalan <64902274+SevvalliThavapalan@users.noreply.github.com>
2025-04-24 09:20:48 +0200

    Update Introduction.py

--- Web_Application/Introduction.py
+++ Web_Application/Introduction.py
@@ -14,8 +14,8 @@ st.title("Introduction")
 
 st.markdown("""
 
-This web application was developed to translate a list of amino acid mutations into DNA oligomers with sgRNA-insert pairs for CRISPR assisted recombineering. It is part of the project "A repository-scale workflow for linking bacterial 
-            genetic variation to phenotypes". The aim of the workflow is to systematically identify, introduce and analyse effects of individual mutations. Purpose of this application is the second step of the workflow, enabling the 
+This web application was developed to translate a list of amino acid mutations into DNA oligomers with sgRNA-insert pairs for CRISPR assisted recombineering. It is part of the project *A repository-scale workflow for linking bacterial 
+            genetic variation to phenotypes*. The aim of the workflow is to systematically identify, introduce and analyse effects of individual mutations. Purpose of this web application is the second step of the workflow, enabling the 
             automated design of sgRNA-insert pairs. The design principles are based on [Garst et al.](https://www.nature.com/articles/nbt.3718) and the adapted version of another study by our [group](https://www.embopress.org/doi/full/10.15252/msb.202311596).
 """)
 
@@ -24,7 +24,7 @@ st.markdown(
     """
     - **Design sgRNA and insert sequences** for a list of amino acid mutations.
     - **Mutagenesis** of a single protein sequence. The resulting table \
-    can be used to design sgRNAs and inserts.
+    can be used to design sgRNA-insert pairs.
     - **Off target finder** for a list of base pairing regions. \
     They are compared to the target genome to find potential binding sites.
     - The **Visualisation** tab can be used to reintroduce read counting tables. \
@@ -46,14 +46,14 @@ st.markdown(
 st.markdown("### Feedback")
 st.markdown("""
 
-We appreciate any feedback or suggestions you can provide. The initial versions of the computational workflows was developed by one person And even though we tried to think of every potential issue and bug there
+We appreciate any feedback or suggestions you can provide. The initial versions of this website was developed by one person, and even though we tried to think of every potential issue and bug there
 is always the possibility that we missed something. If you have any feedback or run into any issues, feel free to open an issue on our [GitHub repository](https://github.com/SevvalliThavapalan/LinkGenVarPhen-Workflow/tree/main).
             """
 )
 
-st.markdown("### LinkMetabolism(LinkLabs)")
+st.markdown("### Link Metabolism")
 st.markdown("""
 - For more about our work, visit our [homepage](https://www.linkmetabolism.com/).
 - Our Github [page](https://github.com/LinkMetabolism)
-- Follow us on [X](https://twitter.com/LinkLabs) for the latest updates.
+- Follow us on [X](https://twitter.com/LinkLabs).
 """)
