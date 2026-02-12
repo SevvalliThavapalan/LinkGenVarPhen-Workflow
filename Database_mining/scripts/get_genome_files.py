78b24ea003f4137efecf7d38d2ae24c8995b1867
SevvalliThavapalan <64902274+SevvalliThavapalan@users.noreply.github.com>
2026-02-12 10:51:12 +0100

    Update get_genome_files.py
    Made the data parsable instead of hard coding

--- Database_mining/scripts/get_genome_files.py
+++ Database_mining/scripts/get_genome_files.py
@@ -3,11 +3,23 @@
 Created on Thu Oct 12 14:23:00 2023""
 """
 import time
+import argparse
+from pathlib import Path
 import wget
 import pandas as pd
 
-isolates = pd.read_csv("ncbi_dataset.tsv", sep='\t')
-assemblies = pd.read_table("assembly_summary_genbank.txt", sep='\t', header=1, low_memory=False)
+parser = argparse.ArgumentParser(description="Download clinical isolate assemblies")
+
+parser.add_argument("--isolates", type=Path, required=True, help="Path to ncbi_dataset.tsv")
+
+parser.add_argument("--assemblies", type=Path, required=True, help="Path to assembly_summary_genbank.txt")
+
+parser.add_argument("--outdir", type=Path, default=Path("clinical"), help="Output directory for downloaded files")
+
+args = parser.parse_args()
+
+isolates = pd.read_csv(args.isolates, sep="\t")
+assemblies = pd.read_table( args.assemblies, sep="\t", header=1, low_memory=False)
 
 clinical_df = isolates.loc[isolates["Isolation type"] == "clinical"]
 
@@ -16,17 +28,17 @@ clinical_isolates = clinical_df["Assembly"].tolist()
 TIME = 2 # 5 seconds
 
 #clinical isolates
-
+args.outdir.mkdir(parents=True, exist_ok=True)
 urls_clinical = []
 for i in assemblies["# assembly_accession"]:
     if i in clinical_isolates: # needs to  be adjusted if clinical or environmental
         urls_clinical.append(assemblies.loc[assemblies["# assembly_accession"]==i,
                                             'ftp_path'].values[0])
 
-OUTPUT_DIR = "clinical"
-for i in urls_clinical:
-    fragments = i.split("/")
-    file_url =  i + "/" + fragments[-1] + "_genomic.gbff.gz"
+
+for url in urls_clinical:
+    fragments = url.split("/")
+    file_url =  url + "/" + fragments[-1] + "_genomic.gbff.gz"
     print(file_url)
-    wget.download(file_url, out=OUTPUT_DIR)
+    wget.download(file_url, out=str(args.outdir))
     time.sleep(TIME)
